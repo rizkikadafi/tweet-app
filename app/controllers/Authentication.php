e0b6cf567cbe59eb5e8707908ebac1778f9e59d5
rizkikadafi <rizkikadafi11@gmail.com>
2023-12-16 10:42:48 +0700

    default to register

--- app/controllers/Authentication.php
+++ app/controllers/Authentication.php
@@ -3,12 +3,11 @@
 class Authentication extends Controller {
   // default method
   public function index() {
-    $this->login();
+    $this->register();
   }
 
   public function register() {
     $data['title'] = 'Register'; // tab title
-    $data['styles'] = ['register.css']; // css
     $this->view('templates/header', $data);
     $this->view('authentication/register');
     $this->view('templates/footer', $data);
--- app/views/authentication/register.php
+++ app/views/authentication/register.php
@@ -1,38 +1,8 @@
-<div class="container">
-    <div class="banner">
-      <img src="<?= BASEURL; ?>/img/logo_unj.png" alt="">
-      <h1>Data Sistem Akademik</h1>
-    </div>
-    <div class="form-login">
-      <form action="" method="post">
-        <h2 class="form-title">Registerasi Siakad</h2>
-        <div class="input-wrapper">
-          <div class="form-input">
-            <img src="<?= BASEURL; ?>/img/account-circle.svg" alt="" class="input-icon">
-            <input type="text" name="fullname" id="fullname" autocomplete="off" required>
-            <span class="placeholder" name="placeholder">Nama Lengkap</span>
-          </div>
-          <div class="form-input">
-            <img src="<?= BASEURL; ?>/img/account-circle.svg" alt="" class="input-icon">
-            <input type="text" name="username" id="username" autocomplete="off" required>
-            <span class="placeholder" name="placeholder">Username</span>
-          </div>
-          <div class="form-input">
-            <img src="<?= BASEURL; ?>/img/password.svg" alt="" class="input-icon">
-            <input type="password" name="password" id="password" autocomplete="off" required>
-            <span class="placeholder" name="placeholder">Password</span>
-          </div>
-          <div class="form-input">
-            <img src="<?= BASEURL; ?>/img/password.svg" alt="" class="input-icon">
-            <input type="password" name="password2" id="password2" autocomplete="off" required>
-            <span class="placeholder" name="placeholder">Konfirmasi password</span>
-          </div>
-          <button type="submit" name="register" class="submit-form">Registerasi</button>
-        </div>
-
-        <div class="login">
-          <p>Sudah punya akun? <a href="<?= BASEURL; ?>/authentication/login">Login</a></p>
-        </div>
-      </form>
+<div class="container text-center">
+  <div class="row min-vh-100 align-items-center">
+    <div class="col">
+      <img src="<?= BASEURL; ?>/img/tweet-logo.png" alt="">
+      <h2>Welcome to TweetApp</h2>
     </div>
   </div>
+</div>
